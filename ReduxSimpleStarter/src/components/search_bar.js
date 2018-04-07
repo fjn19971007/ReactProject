5fcae6b3349864486f9d59268fe12baea80090bf
Jianan Fang <fangx205@umn.edu>
2018-04-07 00:41:49 -0500

    First Commit

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -12,7 +12,7 @@ class SearchBar extends Component {
 		<div>
 			<input 
 			value={this.state.term}
-			onChange={ (event) => this.setState({ term: event}) }/> <br />
+			onChange={ (event) => this.setState({ term: event.target.value}) }/> <br />
 			Value of the input: {this.state.term}
 		</div>
 		);
